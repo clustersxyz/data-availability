e3818b7bb5fc1776a935669b3ab3d0422fce7be2
Zodomo <zodomo@proton.me>
2024-09-06 12:41:17 -0500

    trying to build package

---
+++ rollup.config.js
@@ -0,0 +1,11 @@
+import typescript from '@rollup/plugin-typescript';
+
+export default {
+  input: 'src/index.ts',
+  output: [
+    { file: 'lib/index.cjs', format: 'cjs' },
+    { file: 'lib/index.esm.js', format: 'esm' },
+  ],
+  plugins: [typescript()],
+  treeshake: true,
+};
